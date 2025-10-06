5e868d1be6cc29f8610c95147447719270fa7f6e
robert-portilho <31190637+robert-portilho@users.noreply.github.com>
2025-10-06 02:39:04 +0000

    final

--- app/routers/predict.py
+++ app/routers/predict.py
@@ -1,35 +1,50 @@
-from fastapi import APIRouter, UploadFile, File, HTTPException
+from fastapi import APIRouter, HTTPException
+from pydantic import BaseModel, Field
 import pandas as pd
-from app.services.model_service import load_model, run_model
-from app.utils.data_utils import preprocess_data
-import json
+import joblib
+from pathlib import Path
 
-router = APIRouter(prefix="/predict", tags=["Prediction"])
 
-@router.post("/")
-async def predict(file: UploadFile = File(...)):
-    if not file.filename.endswith(".csv"):
-        raise HTTPException(status_code=400, detail="O arquivo deve ser um CSV.")
+router = APIRouter(prefix="/predict", tags=["Prediction"], include_in_schema=True)
+
+
+# 🔹 Modelo de entrada — define os parâmetros esperados
+class PredictionInput(BaseModel):
+    koi_duration: float = Field(..., description="Duração do trânsito")
+    koi_ror: float = Field(..., description="Razão raio planeta/estrela")
+    koi_srho: float = Field(..., description="Densidade estelar")
+    koi_dicco_msky: float = Field(..., description="Correlação de deslocamento medido no céu")
+    koi_dikco_msky: float = Field(..., description="Correlação de deslocamento intrínseco no céu")
+
+
+@router.post("")
+def predict(input_data: PredictionInput):
+    """
+    Recebe parâmetros numéricos no corpo da requisição e retorna a probabilidade de classificação.
+    """
 
     try:
-        # Lê o arquivo em memória
-        df = pd.read_csv(file.file)
-        df_preprocessed = preprocess_data(df)
 
-        #model = load_model()
-        #predictions = run_model(model, df_preprocessed)
-        # Simulação de previsões para exemplo de exoplabetas (Name, Period, Radius, Mass, Magnitude, Classification)
-        prediction = {
-            "Name": "Test-33D",
-            "Period": 289.9,
-            "Radius": 2.4,
-            "Mass": 36.0,
-            "Magnitude": 11.5,
-            "Classification": "PLANET"
-        }
+        model_path = Path(__file__).resolve().parent.parent / "models" / "final_model.joblib"
+        # 🔸 Carrega o modelo
+        model = joblib.load(model_path)
         
 
-        return prediction
+
+        # 🔸 Cria um DataFrame com os dados recebidos
+        obs = pd.DataFrame([{
+            "koi_duration": input_data.koi_duration,
+            "koi_ror": input_data.koi_ror,
+            "koi_srho": input_data.koi_srho,
+            "koi_dicco_msky": input_data.koi_dicco_msky,
+            "koi_dikco_msky": input_data.koi_dikco_msky
+        }])
+
+        # 🔸 Faz a previsão (probabilidade)
+        result = model.predict_proba(obs)[:, 1]
+        print(result)
+
+        return {"prediction_probability": float(result[0])}
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
