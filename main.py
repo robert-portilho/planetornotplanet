f82664d83759fea768da35711644e355f411ea08
robert-portilho <31190637+robert-portilho@users.noreply.github.com>
2025-10-06 03:05:54 +0000

    teste

--- main.py
+++ main.py
@@ -11,10 +11,10 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"],
+    allow_headers=["*"]
 )
 app.include_router(predict.router)
 
