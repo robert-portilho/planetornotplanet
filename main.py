bd50fa2cd1edcd7f937f9f474c71d03d4039d9e6
robert-portilho <31190637+robert-portilho@users.noreply.github.com>
2025-10-06 03:19:14 +0000

    teste

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
 from app.routers import predict
 
 
